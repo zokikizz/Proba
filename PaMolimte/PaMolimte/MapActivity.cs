f59f352b990faaa1a2f4cef3d983ea347622d32b
DESKTOP-B4LLECO\Kizz <zp.zoki95@gmail.com>
2017-05-14 11:06:59 +0200

    Ubaceni markeri koji mogu da se prevlace i imaju naslov i podnaslov, kao i event koji kada klikes na njih privlizi te njima, setovanje kamere tj updejt kamere kada se alikacija upali da priblizi do odredjenog mesta

--- PaMolimte/PaMolimte/MapActivity.cs
+++ PaMolimte/PaMolimte/MapActivity.cs
@@ -10,6 +10,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
 
 namespace PaMolimte
 {
@@ -17,7 +18,7 @@ namespace PaMolimte
     public class MapActivity : Activity, IOnMapReadyCallback
     {
         private GoogleMap gmap;
-        private MapFragment _mapf;
+        //private MapFragment _mapf;
 
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -48,7 +49,50 @@ namespace PaMolimte
         public void OnMapReady(GoogleMap googleMap)
         {
             gmap = googleMap;
+
+
+            //set marker
+            LatLng latLng = new LatLng(40.776408, -73.970755);
+            LatLng l2 = new LatLng(41, -73);
+
+            //postasvljanje kamere
+            CameraUpdate camera = CameraUpdateFactory.NewLatLngZoom(latLng, 10); // apdejtovace se kamera na mesto gde je latLng i uveca ce se puta 10
+            gmap.MoveCamera(camera);
+
+            
+            MarkerOptions options = new MarkerOptions().SetPosition(latLng).SetTitle("New York").SetSnippet("AKA: Big App;e"); // snippet podnaslov, duzi opis, mogu da se steve i drugi properitiji pr,
+            // set icon = > SetIcon(BitmapDescriptorFactory.DefaultMarker(BitmapDescriptorFactory.HueAzure) menja marker u drugu boju
+
+            //ako hocemo da nam se marker pomera Draggable(true)
+            options.Draggable(true);
+
+            gmap.AddMarker(options); //dodavanje markera latLng
+
+            gmap.AddMarker(new MarkerOptions().SetPosition(l2).SetTitle("Marker 2")); // dodavanje makera 2
+
+            gmap.MarkerClick += Marker_Click;
+
+            gmap.MarkerDragEnd += Map_MarkerDragend;
+
         }
+
+        private void Marker_Click(object sender, GoogleMap.MarkerClickEventArgs e)
+        {
+            LatLng pos = e.Marker.Position;
+            gmap.AnimateCamera(CameraUpdateFactory.NewLatLngZoom(pos, 10));
+        }
+
+        private void Map_MarkerDragend(object sender, GoogleMap.MarkerDragEndEventArgs e)
+        {
+            LatLng l = e.Marker.Position;
+            Console.WriteLine(l.ToString());
+        }
+
+        //to change map view
+        /*
+         * gmap.MapType = GoogleMap.MapTypeNormal; ect
+         * 
+         * */
 
         public void SetUpMap()
         {
